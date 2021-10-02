3f68536783b98865993d2098161df77d42e1b5d6
Molly Jones <molly.jones@spantree.net>
2021-10-02 11:51:56 -0500

    Initial commit

---
+++ tracerybot/alt_grammar.js
@@ -0,0 +1,19 @@
+module.exports = {
+  fly_verbs: [
+    "flying",
+    "soaring",
+    "fluttering",
+    "rising",
+    "ascending",
+    "falling",
+    "floating",
+    "flapping",
+    "whispering",
+  ],
+  prepositions: ["through", "past", "within", "among", "above"],
+  adverbs: ["noiselessly", "silently", "palpably", "smoothly"],
+  spaces: ["ether", "flame", "glow", "plasma", "pitch", "absence"],
+  phrase: [
+    "#fly_verbs.capitalize#, #fly_verbs# \n #adverbs# #prepositions# the #spaces#",
+  ],
+};
